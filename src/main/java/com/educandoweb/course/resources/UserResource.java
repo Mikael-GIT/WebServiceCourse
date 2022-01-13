7b31719c9493ae366c9af3df5f9a41fb9a27fffa
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-01-13 18:11:42 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,18 +1,25 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.Servlet;
 import javax.websocket.server.PathParam;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
@@ -31,5 +38,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        User obj = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
---
+++ src/test/java/com/educandoweb/course/UserService.java
@@ -0,0 +1,21 @@
+package com.educandoweb.course;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void deveCadastrarUmNovoUsuario(){
+	}
+	
+
+}
