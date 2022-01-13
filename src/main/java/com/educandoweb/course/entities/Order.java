064b5ec04be6b6b74db6084bd5fbd6c873a94931
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-01-13 17:48:16 -0300

    Subtotal e total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -58,6 +58,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Double getTotal(){
+		Double sum = 0.0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
+
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -32,6 +32,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal(){
+		return this.price * this.quantity;
+	}
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
